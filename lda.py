f6190fa3486307044636eedf0b61ffa374918396
manik <dharmanik500@gmail.com>
2019-01-13 15:42:35 -0500

    changed order fixed seed saving models

--- lda.py
+++ lda.py
@@ -9,24 +9,42 @@ from nltk.stem.porter import PorterStemmer
 import string
 import gensim
 from gensim import corpora
+from gensim.test.utils import datapath
 import multiprocessing as mp
 
 stop = set(stopwords.words('english'))
-exclude = set(string.punctuation) 
+stop.add("exist")
+stop.add("because")
+stop.add("via")
+stop.add("interest")
+stop.add("therefore")
+stop.add("hence")
+stop.add("this")
+
+
+exclude = set(string.punctuation)
+exclude.add("-")
+exclude.add("_")
+exclude.add(".")
+exclude.add(";")
+
 lemma = WordNetLemmatizer()
 stemmer = PorterStemmer()
 ntopics = 20
-npasses = 100
+npasses = 400
+result_dir="results"
+model_dir="model"
+
 
 # Creating the object for LDA model using gensim library
 Lda = gensim.models.ldamodel.LdaModel
 
 def clean(doc):
-    stop_free = " ".join([i for i in doc.lower().split() if i not in stop])
-    punc_free = ''.join(ch for ch in stop_free if ch not in exclude)
-    lemmatized = " ".join(lemma.lemmatize(word) for word in punc_free.split())
+    punc_free = ''.join(ch for ch in doc if ch not in exclude)
+    lemmatized = " ".join(lemma.lemmatize(word)+" " for word in punc_free.lower().split())
     stemmed = " ".join(stemmer.stem(word) for word in lemmatized.split())
-    return stemmed
+    stop_free = " ".join([i for i in stemmed.split() if i not in stop])
+    return stop_free
 
 def train_model(year_dir):
     # Read and clean data
@@ -40,21 +58,26 @@ def train_model(year_dir):
     doc_term_matrix = [dictionary.doc2bow(doc) for doc in doc_clean]
 
     # Running and Trainign LDA model on the document term matrix.
-    ldamodel = Lda(doc_term_matrix, num_topics=ntopics, id2word = dictionary, passes=npasses)
+    ldamodel = Lda(doc_term_matrix, num_topics=ntopics, id2word = dictionary, passes=npasses, random_state=0)
 
     # Write results
-    with open("./results/"+year_dir+".txt", 'w') as f:
-        for topic in ldamodel.print_topics(num_topics=ntopics, num_words=5):
+    with open("./"+result_dir+"/"+year_dir+".txt", 'w') as f:
+        for topic in ldamodel.print_topics(num_topics=ntopics, num_words=10):
             f.write("#%i: %s\n" % topic)
         print year_dir
+    temp_file = datapath("./"+model_dir+"/"+year_dir)
+    ldamodel.save("./"+model_dir+"/"+year_dir)
 
 def main():
-    if "results" in os.listdir("."): shutil.rmtree("./results")
-    os.mkdir("./results")
+    if result_dir in os.listdir("."): shutil.rmtree("./"+result_dir)
+    os.mkdir("./"+result_dir)
+
+    if model_dir in os.listdir("."): shutil.rmtree("./"+model_dir)
+    os.mkdir("./"+model_dir)
 
     p = mp.Pool(processes=8)
     p.map(train_model, read_bibtex.get_years())
     p.close()
 
-#train_model("2005")
+# train_model("2005")
 main()
